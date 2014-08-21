048c808c96154a2e75326f410fb7bb83aa9982cd
guindilla <guillermo.fernandez.castellanos@gmail.com>
2014-08-21 13:39:54 +0200

    Restructured the code to make it cleaner. Modified the scrape.urls to return exclusively the urls. Added first recollection of urls.

--- munge/getData.R
+++ munge/getData.R
@@ -1,37 +1,64 @@
+################################################################################
+# Load libraries
+################################################################################
 library(httr)
+library(XML)
 
+################################################################################
+# Configuration options
+################################################################################
 authors <- c("kunalj", "tavish")
-url <- "http://www.analyticsvidhya.com/blog/author/"
+blog.url <- "http://www.analyticsvidhya.com/blog/author/"
+limit.date <- "2014-07-07"
 
-get.author.urls <- function(url, author) {
-    # Where to find all articles?
-    my.url <- paste0(url, author)
+################################################################################
+# Helper functions
+################################################################################
+scrape.urls <- function(blog.url, author) {
+    # Identify the author page on the Abalytics Vidhya blog and recursively
+    # download all the posts authored by said writter.
+    #
+    # Args:
+    #   blog.url: URL of the Abalytics Vidhya blog
+    #   author: Name of the author of the posts
+    #
+    # Returns:
+    #   Vector of characters with URLs of all articles
+    
     links <- vector()
-    output <- data.frame()
+
+    my.url <- paste0(blog.url, author)
+    
     repeat{
         content <- content(GET(my.url), as="text")
         parsedHTML <- htmlParse(content, asText=TRUE)
-        links <- xpathSApply(parsedHTML, "//h2[@class='post-title entry-title']/a", xmlGetAttr, 'href')
+        page.links <- xpathSApply(parsedHTML,
+                                  "//h2[@class='post-title entry-title']/a",
+                                  xmlGetAttr, 'href')
         ## Next two lines do the same as the line above, kept for reference only
         # links <- xpathSApply(parsedHTML, "//a[@rel='bookmark']/@href")
         # links <- as.vector(links)
-        dates <- xpathSApply(parsedHTML, "//span[@class='value-title']", xmlGetAttr, 'title')
-        output <- rbind(output, cbind(dates, links))
+        
+        ## This line also identifies the dates of the articles. As this
+        # information will be extracted later on with other information (tags,
+        # etc.) we keep it here for reference purposes only.
+        # dates <- xpathSApply(parsedHTML, "//span[@class='value-title']", xmlGetAttr, 'title')
+        
+        links <- c(links, page.links)
+        
         my.url <- xpathSApply(parsedHTML, "//p[@class='previous']/a", xmlGetAttr, 'href')
         if(length(my.url) == 0) {
             break
         }
     }
-    output$links <- as.character(output$links)
-    output$dates <- as.Date(output$dates)
-    return(output)
+    typeof(links)
+    return(links)
 }
-# List: An ordered collection of objects (components). A list allows you to
-# gather a variety of (possibly unrelated) objects under one name.
-# Use lists here.
-kunal.urls <- get.author.urls(url, "kunalj")
-limit.date <- "2014-07-07"
-kunal.urls.train <- kunal.urls[kunal.urls$dates < limit.date, ]
-kunal.urls.test <- kunal.urls[kunal.urls$dates >= limit.date, ]
 
-
+################################################################################
+# Main program
+################################################################################
+urls <- vector()
+for(a in authors) {
+    urls <- c(urls, scrape.urls(blog.url, a))
+}
